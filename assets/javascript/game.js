0e391dea44bc7473c53b67b1b76dd8a470f35564
Tamami Polk <tamami_polk@hotmail.com>
2019-03-19 15:19:10 -0500

    version 3

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,19 +7,63 @@ function getRandomLetter()
         'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
     var computerRandomLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
     console.log(computerRandomLetter);
+
+    return computerRandomLetter;
 }
 
-function resetPsychicGame()
-{
-    var guessesLeft = 0;
+
+function resetPage(){
+
+    var gamesWon = 0;
+    var gamesLost = 0;
+    var guessesLeft = 9;
     var yourGuessesSoFar = "";
 
-    getRandomLetter();
+    document.getElementById("games-won").innerHTML = "Games Won: " + gamesWon + "<br><br>";
+    document.getElementById("games-lost").innerHTML = "Games Lost: " + gamesLost + "<br><br>";
+    document.getElementById("guesses-left").innerHTML = "Guesses Left: " + guessesLeft + "<br><br>";
+    document.getElementById("guesses-so-far").innerHTML = "Your Guesses So Far: " + yourGuessesSoFar + "<br>";
+
+    var computerLetter = getRandomLetter();
+    console.log(computerLetter);
+
 }
 
 
-var gamesWon;
-var gamesLost;
-var guessesLeft;
-var yourGuessesSoFar;
+
+function matchOrNot(userGuess, computerLetter) {
+
+    console.log("inside matchOrNot function " + computerLetter);
+
+
+    if (computerLetter.toLowerCase() === userGuess.toLowerCase()) {
+
+            gamesWon = gamesWon + 1;
+
+            resetPage();
+
+    }
+        else {
+
+        guessesLeft = guessesLeft - 1;
+
+        if (guessesLeft === 0) {
+            gamesLost = gamesLost + 1;
+            resetPage();
+        }
+    }
+}
+
+
+
+// var userGuess = document.getElementById("userKeyChoice").value;
+// document.getElementById("userKeyChoice").innerHTML = userGuess;
+
+function getUserKey() {
+    var userChoice = document.getElementById("userKeyChoice").value;
+    // document.getElementById("demo").innerHTML = x;
+    console.log(userChoice);
+    matchOrNot(userChoice);
+}
+
 
